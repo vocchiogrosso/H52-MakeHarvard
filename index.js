44450e3967a39cf9e8dc629a6b66642b59012ebb
Vincent Occhiogrosso <16623151+vocchiogrosso@users.noreply.github.com>
2019-02-03 09:20:42 -0500

    2.3.2019.A

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-Webcam.set({
-    width: 600,
-    height: 300,
-    dest_width: 600,
-    dest_height: 300,
-    image_format: 'jpeg',
-    jpeg_quality: 90
-});
-Webcam.attach( '#my_camera' );
+
+function set_camera(){
+    alert("You Must Next Enable The Browser To Access Your Camera.")
+    Webcam.set({
+        width: 400,
+        height: 300,
+        dest_width: 400,
+        dest_height: 300,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+    Webcam.attach( '#my_camera' );
+}
+
 
 
 function take_snapshot() {
@@ -15,4 +20,8 @@ function take_snapshot() {
         // display results in page
         document.getElementById('results').innerHTML = '<h2>Here is your image:</h2>' + '<img src="'+data_uri+'"/>';
     });
+}
+
+function submit(){
+   
 }
